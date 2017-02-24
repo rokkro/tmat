25264d5060d83c897411bdf173457cd1062c98aa
rukkro <rukkro@users.noreply.github.com>
2017-02-24 10:04:53 -0600

    Update main.py

--- src/main.py
+++ src/main.py
@@ -21,6 +21,8 @@ def inputNum(msg, inpt_msg, lim, blank=False): #for typecasting to an int withou
         inpt = input(inpt_msg)
         if inpt == 'p':
             reprint()
+        elif inpt == 'q':
+            quit()
         if inpt == '' and blank:
             break
         try:
@@ -34,89 +36,98 @@ def inputNum(msg, inpt_msg, lim, blank=False): #for typecasting to an int withou
     return None
 
 def mode():
-    i = inputNum("[1] - Scrape tweets.\n"
-          "[2] - Perform Sentiment Analysis.\n"
-          "[3] - Data Presentation.\n"
-          "[4] - List Tweet Collections in DB.\n"
-          "[5] - Purge temporary data\n","*Enter option number or 'p' to re-list options\n>>>",5)
-    if i == 1:
-        scrapeMode()
-    elif i == 2:
-        print(2)
-    elif i == 3:
-        print(3)
-    elif i == 4:
-        print(4)
-    elif i == 5:
-        print(5)
-    elif i == 6:
-        print(6)
+    while True:
+        i = inputNum("[1] - Scrape tweets.\n"
+              "[2] - Perform Sentiment Analysis.\n"
+              "[3] - Data Presentation.\n"
+              "[4] - List Tweet Collections in DB.\n"
+              "[5] - Purge temporary data\n","*Enter option number, 'p' to re-print, or 'q' to quit.\n>>>",5)
+        if i == 1:
+            scrapeMode()
+        elif i == 2:
+            print(2)
+        elif i == 3:
+            print(3)
+        elif i == 4:
+            print(4)
+        elif i == 5:
+            print(5)
+        elif i == 6:
+            print(6)
 
 def scrapeMode():
     s = twitter.Setup()
     print(color.YELLOW)
-    #s.mongo_connect() #CONNECTS TO MONGODB
+    s.mongo_connect() #CONNECTS TO MONGODB
     print(color.END)
     search = s.search()
     limit = s.limit()
     while True:
-        j = inputNum(
-            "[1] - Search = '" + search + "'\n[2] - Limit = " + str(limit) + "\n[3] - Temporary Collection = " + str(
-                s.temp) +
+        i = inputNum(
+            "[1] - Search = '" + search + "'\n[2] - Limit = " + str(limit) + "\n[3] - Temporary Collection = " + str(s.temp) +
             "\n[4] - Image Filtering and Analysis = " + str(s.img) + "\n[5] - Database Name = '" + s.db_name +
             "'\n[6] - Collection Name = '" + s.coll_name + "'\n[7] - "
-                                                           "MongoDB connected = " + color.YELLOW + str(s.connected) + color.END,
+            "MongoDB connected = " + color.YELLOW + str(s.connected) + color.END,
             "*Enter an option to change or press 'Enter' to begin if MongoDB is connected.\n>>>", 7, True)
-        if j == None and s.connected:
+        if i == None and s.connected:
             twitter.stream(search, limit, s.coll_name, s.db_name)
             break
-        elif j == 1:
+        elif i == 1: #SEARCH
             search = s.search()
-        elif j == 2:
+        elif i == 2: #SET LIMIT
             limit = s.limit()
-        elif j == 3:
+        elif i == 3: #TEMP OR PERMANENT
             if s.temp == False:
                 print(color.YELLOW, "**Collection marked as Temporary.**", color.END)
                 s.temp = True
             else:
                 print(color.YELLOW, "**Collection marked as Permanent.**", color.END)
                 s.temp = False
-        elif j == 4:
+        elif i == 4: #IMAGE ANALYSIS
             s.img = True
-        elif j == 5:
-            inpt = input(
-                "Enter a new name for the database, currently '" + s.db_name + "'. Leave blank to cancel. Spaces will be removed.\n>>>").replace(" ","")
+
+        elif i == 5: #DATABASE NAME
+            inpt = input("Enter a new name for the database, currently '" + s.db_name + "'. Leave blank to cancel. Spaces will be removed.\n>>>").replace(" ","")
             print(inpt)
             if inpt == '' or inpt == s.db_name:
                 continue
-            print(color.YELLOW, "**Database changed from '" + s.db_name + "' to '" + inpt + "'.", end='')
+            print(color.YELLOW, "**Database changed from '" + s.db_name + "' to '" + inpt + "'.", end=' ')
             s.db_name = inpt
             if s.connected:
                 if inpt in s.dbname_list:
-                    print(inpt, "already exists. New tweets will be added to existing.**", color.END)
+                    print("'" + inpt + "' already exists. New tweets will be added to existing.**", color.END)
                 else:
-                    print("New database", inpt, "will be created.**", color.END)
+                    print("New database '" + inpt + "' will be created.**", color.END)
             else:
                 print("**", color.END)
-        elif j == 6:
+
+        elif i == 6: #COLLECTION NAME
             inpt = input(
                 "Enter a new name for this collection, currently '" + s.coll_name + "'. Leave blank to cancel.\nAdd [dt] to name to add date + time to end.\n>>>").strip()
             if inpt == '' or inpt == s.coll_name:
                 continue
-            print(color.YELLOW, "**Collection name changed from '" + s.coll_name + "' to '", end='')
+            coll_old = s.coll_name
             if '[dt]' in inpt:
                 inpt = inpt.replace('[dt]', '').strip()
-                print(inpt + s.dt + "'.**", color.END)
                 s.coll_name = inpt + s.dt
             else:
-                print(inpt + "'.**", color.END, end=' ')
                 s.coll_name = inpt
-            if s.connected and s.coll_name in s.collname_list:
-                print(color.YELLOW, s.coll_name + " already exists. Tweets will be added to existing.")
-        elif j == 7:
+            print(color.YELLOW, "**Collection changed from '" + coll_old + "' to '" + s.coll_name + "'.", end=' ')
+            if s.connected:
+                if s.coll_name in s.get_collections():
+                    print("'" + s.coll_name + "' already exists. Tweets will be added to existing.**",color.END)
+                else:
+                    print("New collection '" + s.coll_name + "' will be created.**",color.END)
+            else:
+                print("**",color.END)
+
+        elif i == 7: #CONNECTION TO MONGODB
             print(color.YELLOW)
             s.mongo_connect()
             print(color.END)
 
 if __name__ == "__main__":
-    mode()
+    try:
+        mode()
+    except KeyboardInterrupt:
+        pass
