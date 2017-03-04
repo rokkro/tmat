94efeab1f06835ea18e8b58c74a55d58a4bf1757
rukkro <rukkro@users.noreply.github.com>
2017-03-04 16:31:23 -0600

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,17 +1,11 @@
 import twitter
 
 class color:
-    PURPLE = '\033[95m'
-    CYAN = '\033[96m'
-    DARKCYAN = '\033[36m'
-    BLUE = '\033[94m'
-    GREEN = '\033[92m'
     YELLOW = '\033[93m'
-    RED = '\033[91m'
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
     END = '\033[0m'
-# http://stackoverflow.com/a/17303428 for pretty font coloring
+# http://stackoverflow.com/a/17303428 for pretty font
 
 def color_msg(function): #simple message coloring
     print(color.YELLOW)
@@ -143,3 +137,18 @@ if __name__ == "__main__":
         mode()
     except KeyboardInterrupt:
         pass
+
+    '''
+            #4:
+                #mongo(s)
+                if s.connected:
+                    while True:
+                        print("DATABASES:")
+                        for j in range(0,len(s.dbname_list)):
+                            print('[' + str(j) + '] - ' + s.dbname_list[j] )
+                        inp = int(input("Enter DB number:"))
+                        s.db_name = s.dbname_list[inp]
+                        print("COLLECTIONS")
+                        for j in range(0,len(s.get_collections())):
+                            print('[' + str(j) + '] - ' + s.get_collections()[j])
+    '''
