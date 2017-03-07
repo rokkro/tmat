a7b268950a2dc4ebc7244f8d1624931d569a6c82
rukkro <rukkro@users.noreply.github.com>
2017-03-06 19:03:46 -0600

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,13 +1,13 @@
-import twitter
+import twitter, mongo
 
 class color:
     YELLOW = '\033[93m'
     BOLD = '\033[1m'
     END = '\033[0m'
 
-def get_input(msg,inpt_msg,lim,blank=False):
+def get_input(msg, inpt_msg, lim, blank=False):
     while True:
-        print("" + len(inpt_msg) * '-')
+        print(len(inpt_msg) * '-')
         print(msg)
         print(len(inpt_msg) * '-')
         i = input(color.BOLD + inpt_msg + color.END)
@@ -36,29 +36,32 @@ def main_menu():
     }
     while True:
         i = get_input("[1] - Scrape tweets.\n"
-              "[2] - Perform Sentiment Analysis.\n"
-              "[3] - Data Presentation.\n"
-              "[4] - List Databases and Collections.\n"
-              "[5] - Purge temporary data","*Enter option number or [q] - quit.\n>>>",5)
+                      "[2] - Perform Sentiment Analysis.\n"
+                      "[3] - Data Presentation.\n"
+                      "[4] - List Databases and Collections.\n"
+                      "[5] - Purge temporary data", "*Enter option number or [q] - quit.\n>>>", 5)
         menu[i]()
 
+
 ########################
-def scrape_menu(): #menu for setting up tweet scraping
+def scrape_menu():  # menu for setting up tweet scraping
     s = twitter.Setup()
-    print(color.YELLOW,end='')
-    s.mongo_handler()
-    print(color.END,end='')
+    print(color.YELLOW, end='')
+    mongo.mongo_handler()
+    print(color.END, end='')
     search = s.search()
     limit = s.limit()
     while True:
         selection = get_input("[1] - Search = '" + str(s.term).strip('\'[]\'') + "'\n[2] - Limit = " + str(s.lim) +
-            "\n[3] - Temporary Collection = "+str(s.temp) + "\n[4] - Image Filtering and Analysis = " + str(s.img) +
-            "\n[5] - Database Name = '" +s.db_name + "'\n[6] - Collection Name = '" + s.coll_name +
-            "'\n[7] - Tweet Similarity Threshold = " + str(s.similarity) +
-            "\n[8] - MongoDB Connected = " + color.YELLOW +str(s.connected) + color.END,
-            "*Enter option number or: [Enter] - begin if MongoDB is connected, [r] - return.""\n>>>", 8, True)
-        if selection == None and s.connected:
-            twitter.stream(search, limit, s.coll_name, s.db_name,s.temp,s.similarity)
+                              "\n[3] - Temporary Collection = " + str(
+            s.temp) + "\n[4] - Image Filtering and Analysis = " + str(s.img) +
+                              "\n[5] - Database Name = '" + s.db_name + "'\n[6] - Collection Name = '" + s.coll_name +
+                              "'\n[7] - Tweet Similarity Threshold = " + str(s.similarity) +
+                              "\n[8] - MongoDB Connected = " + color.YELLOW + str(mongo.connected) + color.END,
+                              "*Enter option number or: [Enter] - begin if MongoDB is connected, [r] - return.""\n>>>",
+                              8, True)
+        if selection == None and mongo.connected:
+            twitter.stream(search, limit, s.coll_name, s.db_name, s.temp, s.similarity)
             break
 
         elif selection == 'r':
@@ -73,16 +76,16 @@ def scrape_menu(): #menu for setting up tweet scraping
             print(color.YELLOW + "Limit changed to " + str(s.lim) + "." + color.END)
 
         elif selection == 3:
-            print(color.YELLOW,end='')
+            print(color.YELLOW, end='')
             if s.temp == False:
                 print("Collection will be marked as Temporary.")
                 s.temp = True
             else:
                 print("Collection will be marked as Permanent.")
                 s.temp = False
-            print(color.END,end='')
+            print(color.END, end='')
 
-        elif selection == 4: #not yet implemented
+        elif selection == 4:  # not yet implemented
             if s.img:
                 s.img = False
             else:
@@ -91,47 +94,47 @@ def scrape_menu(): #menu for setting up tweet scraping
         elif selection == 5:
             while True:
                 inpt = input(color.BOLD + "Enter a new name for the database, currently '" + s.db_name +
-                    "'. Leave blank to cancel. ""Spaces will be removed.\n>>>" + color.END).replace(" ","")
+                    "'. Leave blank to cancel. ""Spaces will be removed.\n>>>" + color.END).replace(" ", "")
                 if inpt == '' or inpt == s.db_name:
                     break
                 print(color.YELLOW + "Database changed from '" + s.db_name + "' to '" + inpt + "'.")
                 s.db_name = inpt
-                if s.connected:
-                    if inpt in s.dbname_list:
+                if mongo.connected:
+                    if inpt in s.get_dbnames():
                         print("'" + inpt + "' already exists. New tweets will be added to existing.")
                     else:
                         print("New database '" + inpt + "' will be created.")
-                print(color.END,end='')
+                print(color.END, end='')
                 break
 
         elif selection == 6:
             while True:
                 inpt = input(color.BOLD + "Enter a new name for this collection, currently '" + s.coll_name +
                     "'. Leave blank to cancel.\nPut [dt] in name to insert date + time.\n>>>" + color.END).strip()
-                if inpt == '' or inpt == s.coll_name: #If blank or collection name is same
+                if inpt == '' or inpt == s.coll_name:  # If blank or collection name is same
                     break
-                if '[dt]' in inpt: #inserting and replacing [dt] with date/time
-                    s.coll_name = inpt.replace('[dt]',s.dt).strip()
+                if '[dt]' in inpt:  # inserting and replacing [dt] with date/time
+                    s.coll_name = inpt.replace('[dt]', s.dt).strip()
                 else:
                     s.coll_name = inpt
                 print(color.YELLOW + "Collection changed to '" + s.coll_name + "'.")
-                if s.connected:
+                if mongo.connected:
                     if s.coll_name in s.get_collections():
                         print("'" + s.coll_name + "' already exists. New tweets will be added to existing.")
                     else:
                         print("New collection will be created.")
-                print(color.END,end='')
+                print(color.END, end='')
                 break
-                
+
         elif selection == 7:
             while True:
                 inpt = input(color.BOLD + "Enter a new similarity threshold - 0.0 to 1.0. Higher value = filter out "
-                             "higher similarity. Leave blank to cancel.\n>>>"+ color.END)
+                    "higher similarity. Leave blank to cancel.\n>>>" + color.END)
                 if inpt == '' or inpt == s.similarity:
                     break
                 try:
                     inpt = float(inpt)
-                    if inpt <=1.0 and inpt >= 0:
+                    if inpt <= 1.0 and inpt >= 0:
                         s.similarity = inpt
                     else:
                         raise ValueError
@@ -142,9 +145,9 @@ def scrape_menu(): #menu for setting up tweet scraping
                     continue
 
         elif selection == 8:
-            print(color.YELLOW,end='')
-            s.mongo_handler()
-            print(color.END,end='')
+            print(color.YELLOW, end='')
+            mongo.mongo_handler()
+            print(color.END, end='')
 ########################
 
 if __name__ == "__main__":
