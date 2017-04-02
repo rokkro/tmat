a5df0e6f3380e842aca1ee694bdafdf47a544a45
rukkro <rukkro@users.noreply.github.com>
2017-04-01 21:27:58 -0500

    Create menu_analysis.py

---
+++ src/analysis/menu_analysis.py
@@ -0,0 +1,20 @@
+from menu_provide import get_input,get_list
+from analysis import sentiment
+
+def menu_sentiment():
+    inpt = get_input("[1] - Run initial setup.\n[2] - Choose a collection to analyze.",
+                     "Enter an option number or [r] - return.\n>>>",2)
+    if inpt=='r':
+        return
+
+    def sub_analysis():
+        i = get_list()
+        if i==None:
+            return
+        sentiment.analyze(i)
+
+    menu = {
+        1: sentiment.initialize,
+        2:sub_analysis,
+    }
+    menu[inpt]()
