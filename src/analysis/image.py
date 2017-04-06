482b2a265f9dae3140a361c6be99ca4b257ad8f4
rukkro <rukkro@users.noreply.github.com>
2017-04-06 15:42:57 -0500

    Update image.py

--- src/analysis/image.py
+++ src/analysis/image.py
@@ -17,40 +17,46 @@ def emotion(img):
     url = 'https://api.kairos.com/v2/media'
     with open(img,'rb') as img:
         response = requests.post(url, files={'source': img}, data={'timeout':60}, headers=auth_headers)
-    print(response.json())
+    if config.verbose:
+        print(response.json())
     return response.json()
 
 def detect(img):
     url = 'https://api.kairos.com/detect'
     response = requests.post(url,json={'image':image_base64(img)},headers=auth_headers)
-    print(response.json())
+    if config.verbose:
+        print(response.json())
     return response.json()
 
 #https://dev.twitter.com/basics/user-profile-images-and-banners
 def insert_data(coll):
+    success = 0
+    print("Running Image analysis...")
     cursor = coll.find({})  # finds all documents in collection
     for i in cursor:  # loop through those
         profile_pic =i['user']['profile_image_url_https'].replace("_normal","")
         response = requests.get(profile_pic)
         if response.status_code == 404 or response.status_code == 403: #dead links to images
             continue
-        #print(response.headers)
-        #print(response)
         if not i['user']['default_profile_image'] and 'default_profile' not in profile_pic: #filter both default pics
-            print(profile_pic + " " + i['user']['screen_name'])
+            if config.verbose:
+                print(profile_pic + " " + i['user']['screen_name'])
             img = get_image(response)
             det = detect(img)
             if 'Errors' in det:
-                print("Error:", det['Errors'][0]['ErrCode'] , "-", det['Errors'][0]['Message'])
+                print("Error:", det['Errors'][0]['ErrCode'] , "-", det['Errors'][0]['Message'] + ". Moving onto the next...")
                 continue
-            emo =emotion(img)
 
+            emo =emotion(img)
             coll.update_one({'_id': i.get('_id')}, {'$set': {
                 "face": {
                     "emotion":emo,
                     "detection": det
                 }}})
+            success+=1
         else:
-            #print(profile_pic+ " DEFAULT!" + " " + i['user']['screen_name'])
+            if config.verbose:
+                print(profile_pic+ " DEFAULT PICTURE, IGNORED." + " " + i['user']['screen_name'])
             continue
     os.remove("ta-image.jpg")
+    print("Finished: " + str(success) + " of " + str(cursor.count()) + " successfully processed and inserted!")
