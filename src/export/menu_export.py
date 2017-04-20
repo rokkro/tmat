9c204e6117ee5dc484164d74586dac24dc25cd90
rukkro <rukkro@users.noreply.github.com>
2017-04-20 10:18:31 -0500

    Create menu_export.py

---
+++ src/export/menu_export.py
@@ -0,0 +1,23 @@
+from display import select_coll, get_input
+
+def menu_filter_sentiment():
+
+def menu_filter_image():
+
+def menu_filter_tweets():
+
+
+def menu_export():
+    coll = select_coll()
+    if coll == None:
+        return
+    while True:
+        inpt = get_input("[1] - Set Tweet Filters.\n[2] - Set Sentiment Value Filters."
+                         "\n[3] - Set Image Analysis Filters.","*Set filters or: [Enter] - export to csv, [r] -return",3)
+
+        menu = {
+            1: menu_filter_tweets,
+            2: menu_filter_sentiment,
+            3: menu_filter_image,
+        }
+        menu[inpt]()
