e2d309ebebe481f5ba81071a79c9d63351dae0e8
Rukkro <darrenew@gmail.com>
2017-04-21 22:04:28 -0500

    Writing headers

--- src/exp_csv.py
+++ src/exp_csv.py
@@ -1,15 +1,32 @@
-from display import get_coll, get_menu, color
-from csv import DictWriter, DictReader
+from display import get_coll, color
+from csv import writer
 
-def setup():
-    headers = [
-        'Username', 'Tweet Date', 'Tweet Content','Favorites','Retweets',
-        'Sentiment Pos','Sentiment Neu','Sentiment Neg','Sentiment Comp',
-        'Emotion','Race','Age','Gender','Language','Country','City'
-    ]
+#permissionError when file is open elsewhere
+def write_data(fname):
     coll = get_coll()
     if coll == None:
         return
+    headers = [
+        'Username', 'Tweet Date', 'Tweet Content','Favorites','Retweets',
+        'Sentiment Pos','Sentiment Neu','Sentiment Neg','Sentiment Comp',
+        'Emotion','Race','Age','Gender','Tweet Language','Country','City',
+        'Verified', 'Followers', 'Following'
+    ]
+
+    with open(fname,'w',newline='') as out_file:
+        w = writer(out_file, dialect='excel')
+        w.writerow(headers)
+        #begin grand loop
+        all = coll.find({})
+        for i in all:
+            username = i.find()
+
+
+
+        w.writerow(['test'])
+
+
+def setup():
     fname = input(color.BOLD + "*Please enter a filename. A .csv extension will be added.\n"
                                "Leave blank to cancel.\n>>>" + color.END).replace(" ","")
     if fname == '':
@@ -17,9 +34,5 @@ def setup():
         return
     if ".csv" not in fname:
         fname = fname + ".csv"
-    try:
-        out_file=open(fname,'w')
-        writer = DictWriter(f=out_file,fieldnames=headers)
-        writer.writeheader()
-    except BaseException as e:
-        print("Error:",e)
+    write_data(fname)
+
