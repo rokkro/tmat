29eab04ca027ee392c106e09380466d86b788289
Rukkro <darrenew@gmail.com>
2017-04-23 12:14:14 -0500

    updated csv

--- src/exp_csv.py
+++ src/exp_csv.py
@@ -25,12 +25,14 @@ list_guide = [  # u=user general, t=tweet general, s=sentiment, em=emotions,eth=
     ['u', 'user', 'friends_count'],  # following
     ['t', 'created_at'], ['t', 'text'], ['t', 'lang'], ['t', 'favorite_count'], ['t', 'retweet_count'],
     ['s', 'sentiment', 'pos'], ['s', 'sentiment', 'neu'], ['s', 'sentiment', 'neg'], ['s', 'sentiment', 'compound'],
+
     ['em', 'face', 'emotion', 'frames', 0, 'people', 0, 'emotions', 'anger'],  # anger
     ['em', 'face', 'emotion', 'frames', 0, 'people', 0, 'emotions', 'disgust'],  # disgust
     ['em', 'face', 'emotion', 'frames', 0, 'people', 0, 'emotions', 'fear'],  # fear
     ['em', 'face', 'emotion', 'frames', 0, 'people', 0, 'emotions', 'joy'],  # joy
     ['em', 'face', 'emotion', 'frames', 0, 'people', 0, 'emotions', 'sadness'],  # sadness
     ['em', 'face', 'emotion', 'frames', 0, 'people', 0, 'emotions', 'surprise'],  # surprise
+
     ['eth', 'face', 'detection', 'images', 0, 'faces', 0, 'attributes', 'asian'],  # asian
     ['eth', 'face', 'detection', 'images', 0, 'faces', 0, 'attributes', 'hispanic'],  # hispanic
     ['eth', 'face', 'detection', 'images', 0, 'faces', 0, 'attributes', 'other'],  # other
@@ -49,22 +51,47 @@ def write_data(fname):
     with open(fname,'w',newline='',encoding='utf-8') as out_file:
         w = writer(out_file, dialect='excel')
         w.writerow(headers)
-        def error_slap(cursor,list): #handles non existant key errors, replacing them with the right stuff
+
+        def error_slap(cursor,list,tag): #handles non existant key errors, replacing them with the right stuff
             try:
-                for i in range(1,len(list)): #loop through the list
-                    cursor = cursor[list[i]] #keep accessing the next sub element in document, till reach the final key
-                #print(cursor)
-                data.append(cursor)
+                if list[0] == tag:
+                    for i in range(1,len(list)): #loop through the sub list
+                        cursor = cursor[list[i]] #keep accessing the next sub element in document, till reach the final key
+                    #print(cursor)
+                    data.append([list[0],cursor])
             except Exception as e:
-                print("Error:",e)
-                data.append("")
+                #print("Error:",e)
+                data.append(["",""])
 
 
         all = coll.find({})
-        ethbiggest = embiggest = epgap =  0
-        for k,i in enumerate(all):
-            for item in list_guide:
-                error_slap(i,item)
+        for k,i in enumerate(all): #loop through all the documents.
+            # k=row number, so go from k * len(list_guide) to k*len(list_guide)+len(list_guide) for CURRENT ROW DATA
+            biggest = 0
+            r = []
+            for item in list_guide: #creating a SINGLE row of data with specified tag group
+                error_slap(i,item,"u")
+                error_slap(i, item, "t")
+                error_slap(i, item, "s")
+                error_slap(i,item,"em")
+                error_slap(i, item, "eth")
+                error_slap(i, item, "ep")
+
+            for j in range(len(list_guide)):
+                #print(j,data[j])
+                if "em" in data[j]:
+                    r.append(j)
+                    if float(data[j][1]) > biggest:
+                        biggest = data[j][1]
+                #print(data)
+            if len(r) > 0:
+                for s in range(1,len(r)):
+                    #print(r[s], "DELETING",data[r[s]-s+1])
+                    del data[r[s]-s+1]
+                #print("DATA SUV ZERO",data[r[0]][1])
+                data[r[0]][1] = biggest
+                #print(r[0][1])
+            '''
             em = find_tag("em")
             for emotion in range(em[0],em[len(em)-1]):
                 try:
@@ -72,43 +99,42 @@ def write_data(fname):
                        embiggest = data[emotion]
                 except (ValueError,TypeError):
                     continue
-
-            eth = find_tag("eth")
-            for ethnicity in range(eth[0],eth[len(eth)-1]):
+            del data[em[0]:em[len(em) - 1]]
+            data[em[0]] = embiggest
+            offset+=em[len(em)-1]-em[0]
+            eth = find_tag("eth",em[0],offset)
+            for ethnicity in range(eth[0],eth[len(eth)-1]+1):
                 try:
                     if float(data[ethnicity]) > ethbiggest:
                         ethbiggest = data[ethnicity]
                 except (ValueError,TypeError):
                     continue
-
-            ep = find_tag("ep")
-            for eyes in range(ep[0],ep[len(ep)-1]):
+            del data[eth[0]:eth[len(eth) - 1]]
+            data[eth[0]] = ethbiggest
+            offset+=eth[len(eth)-1]-eth[0]
+            ep = find_tag("ep",eth[0],offset)
+            for eyes in range(ep[0],ep[len(ep)-1]+1):
                 try:
                     epgap = data[ep[0]] - data[ep[len(ep)-1]]
                 except (ValueError, TypeError):
                     continue
-
             del data[ep[0]:ep[len(ep) - 1]]
             data[ep[0]] = epgap
-            del data[eth[0]:eth[len(eth)-1]]
-            data[eth[0]] = ethbiggest
-            del data[em[0]:em[len(em)-1]]
-            data[em[0]] = embiggest
-
-
+            '''
             w.writerow(data)
-            for i in data:
-                print(i)
-            ethbiggest = 0
-            embiggest = 0
-            epgap = 0
-            data[:] = []
 
-def find_tag(tag): #basic tag range
+            #for i in data:
+            #    print(i)
+            data[:] = []
+            r[:] = []
+def find_tag(tag,upper=0,offset=0): #basic tag range
     r = []
     for j,i in enumerate(list_guide):
         if i[0] == tag:
-            r.append(j)
+            if j >=upper:
+                r.append(j-offset)
+            else:
+                r.append(j)
     if len(r) > 0:
         return r
     else:
