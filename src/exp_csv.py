cefd45f3540dd452a4d2bd7c124dacc606ea9995
Rukkro <darrenew@gmail.com>
2017-04-24 15:01:04 -0500

    Updated exp_csv

--- src/exp_csv.py
+++ src/exp_csv.py
@@ -1,20 +1,17 @@
 from display import get_coll, color
 from csv import writer
 
-#permissionError when file is open elsewhere
-headers = [
-    'Username','User Age', 'Age Group', 'Glasses', 'User Gender','Lips', 'Glances', 'Dwell',
-    'Attention', 'Blinking', 'User Country', 'User City',
-    'User Verified', 'User Followers', 'User Following',
-    'Tweet Date', 'Tweet Content', 'Tweet Language', 'Tweet Favorites', 'Tweet Retweets',
-    'Sentiment Pos', 'Sentiment Neu', 'Sentiment Neg', 'Sentiment Comp',
-    'User Emotion', 'User Ethnicity',  'Eye Gap',
-]
-def write_data(fname):
-    coll = get_coll()
-    if coll == None:
-        return
+def write_data(fname,coll):
+    headers = [
+        'Username', 'User Age', 'Age Group', 'Glasses', 'User Gender', 'Lips', 'Glances', 'Dwell',
+        'Attention', 'Blinking', 'User Country', 'User City',
+        'User Verified', 'User Followers', 'User Following',
+        'Tweet Date', 'Tweet Content', 'Tweet Language', 'Tweet Favorites', 'Tweet Retweets',
+        'Sentiment Pos', 'Sentiment Neu', 'Sentiment Neg', 'Sentiment Comp',
+        'User Emotion', 'User Ethnicity', 'Eye Gap',
+    ]
     data = []
+
     with open(fname,'w',newline='',encoding='utf-8') as out_file:
         w = writer(out_file, dialect='excel')
         w.writerow(headers)
@@ -58,8 +55,8 @@ def write_data(fname):
 
         all = coll.find({})
         for iterator,doc in enumerate(all): #far less convoluted than i originally had it set up
-            set_value(doc,[['user'],['screen_name']])
-            set_value(doc,[['face'],['detection'],['images'],[0],['faces'],[0],['attributes'],['age']])
+            set_value(doc,[['user'],['screen_name']]) #@username
+            set_value(doc,[['face'],['detection'],['images'],[0],['faces'],[0],['attributes'],['age']]) #kairos age
             set_value(doc,[['face'],['emotion'],['frames'],[0],['people'], [0], ['demographics'], ['age_group']])  # age group
             set_value(doc,[['face'],['detection'],['images'], [0], ['faces'], [0], ['attributes'], ['glasses']])  # glasses
             set_value(doc,[['face'], ['detection'], ['images'], [0], ['faces'], [0], ['attributes'], ['gender'],['type']])  # gender
@@ -68,16 +65,16 @@ def write_data(fname):
             set_value(doc,[['face'],['emotion'],['frames'], [0], ['people'], [0], ['tracking'], ['dwell']])  # dwell
             set_value(doc,[['face'], ['emotion'], ['frames'], [0], ['people'], [0], ['tracking'], ['attention']]) #attention
             set_value(doc,[['face'], ['emotion'], ['frames'], [0], ['people'], [0], ['tracking'], ['blink']]) #blinking
-            set_value(doc,[['place'], ['country']])
-            set_value(doc,[['place'], ['name']])
-            set_value(doc,[['user'], ['verified']])
+            set_value(doc,[['place'], ['country']]) #country
+            set_value(doc,[['place'], ['name']]) #city
+            set_value(doc,[['user'], ['verified']]) #verfied
             set_value(doc,[['user'], ['followers_count']]) #followers
             set_value(doc,[['user'], ['friends_count']]) #following
             set_value(doc,[['created_at']]) #tweet date
-            set_value(doc, [['text']])
-            set_value(doc, [['lang']])
+            set_value(doc, [['text']]) #tweet content
+            set_value(doc, [['lang']]) #tweet language
             set_value(doc,[['favorite_count']]) #will be 0 unless historical tweet
-            set_value(doc,[['retweet_count']])
+            set_value(doc,[['retweet_count']]) #^
             set_value(doc,[['sentiment'],['pos']])
             set_value(doc, [['sentiment'],['neu']])
             set_value(doc,[['sentiment'],['neg']])
@@ -102,16 +99,22 @@ def write_data(fname):
             }
             name, biggest = get_biggest(ethnicity)
             data.append(name)
+
             eyegap = {
                 "leftCenterX":set_value(doc,[['face'], ['detection'], ['images'], [0], ['faces'], [0], ['leftEyeCenterX']],False),
                 "rightCenterX":set_value(doc,[['face'], ['detection'], ['images'], [0], ['faces'], [0], ['rightEyeCenterX']],False),
             }
             diff = get_difference(eyegap)
             data.append(diff)
+
             w.writerow(data)
             data[:] = []
 
 def setup():
+    coll = get_coll()
+    if coll == None:
+        return
+
     fname = input(color.BOLD + "*Please enter a filename. A .csv extension will be added.\n"
                                "Leave blank to cancel.\n>>>" + color.END).replace(" ","")
     if fname == '':
@@ -119,9 +122,10 @@ def setup():
         return
     if ".csv" not in fname:
         fname = fname + ".csv"
+
     try:
-        write_data(fname)
+        write_data(fname,coll)
     except PermissionError:
-        print(color.YELLOW + "Permission Error: Check if it's open in another program\nor if you have "
+        print(color.YELLOW + "Permission Error: Check if the specified file is open in another program\nand if you have "
                              "permission to create files here." + color.END)
         return
