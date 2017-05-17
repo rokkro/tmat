cb44c2a0a2b130df5572f3474202fefdacdc9b64
Rukkro <darrenew@gmail.com>
2017-05-17 17:52:15 -0500

    Finalize historical tweets

--- src/display.py
+++ src/display.py
@@ -7,8 +7,10 @@ class Color:
     END = '\033[0m'
 
 def header(text):
-    print(Color.CYAN + ('-' * int((40 - len(text)) / 2)) + Color.BOLD +
-          text + Color.CYAN + ('-' * int((40 - len(text)) / 2)) + Color.END)
+    print(Color.CYAN  + Color.BOLD + ('-' * int((40 - len(text)) / 2)) + Color.BOLD +
+          text + Color.CYAN + Color.BOLD + ('-' * int((40 - len(text)) / 2)) + Color.END)
+def footer():
+    print(Color.CYAN + Color.BOLD + '-' * 40 + Color.END)
 
 def get_menu(head,menu, inpt_msg, items=None):
     while True:
@@ -16,8 +18,7 @@ def get_menu(head,menu, inpt_msg, items=None):
             header(head)
             for num, i in enumerate(menu):
                 print("[" + Color.YELLOW + str(num + 1) + Color.END + "] - " + i)
-
-        print(Color.CYAN + '-' * 40 + Color.END)
+            footer()
         i = input(Color.BOLD + inpt_msg + Color.END).replace(" ", "")
         if i == 'q':
             quit()
@@ -44,6 +45,7 @@ def get_db():
     for j, k in enumerate(mongo.get_dbnames(), 1):  # start at 1
         print("[" + Color.YELLOW + str(j) + Color.END + "] - '" + k + "' (" + str(
             len(mongo.get_collections(k))) + ")")  # print databases
+    footer()
     inpt = get_menu("",None, "*Select a db to view collections or [r] - return.\n>>>", len(mongo.get_dbnames()))
     if inpt == 'r' or inpt == '':
         return None
@@ -66,6 +68,7 @@ def get_coll():
               ("(TEMP)" if tmp.count() > 0 else ""))
         tmp.close()
         doc_count.close()
+    footer()
     inpt = get_menu("",None, "*Select a collection or [r] - return.\n>>>", len(coll))
 
     if inpt == 'r' or inpt == '':
